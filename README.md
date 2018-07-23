832ac5123e4514dcf0feaa5ce0724996f2b170e8
Rog <alexanderogov@gmail.com>
2018-07-24 02:45:03 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# CRUD_App_demo
+Build a Basic CRUD App with Angular 5.0 and Spring Boot 2.0 (I'm having fun)
---
+++ client/src/app/shared/car/car.service.ts
@@ -0,0 +1,25 @@
+/*
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CarService {
+
+  constructor() { }
+}
+*/
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {Observable} from "rxjs/index";
+
+@Injectable()
+export class CarService {
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAll(): Observable<any> {
+    return this.http.get('//localhost:8080/cool-cars');
+  }
+}
